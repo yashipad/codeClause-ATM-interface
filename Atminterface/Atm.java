8af89b895bb2efc2447221060fe06776187776bf
yashipad <117570673+yashipad@users.noreply.github.com>
2023-05-29 23:46:10 +0530

    Add files via upload

---
+++ Atminterface/Atm.java
@@ -0,0 +1,13 @@
+package Atminterface;
+
+import java.io.IOException;
+
+public class Atm extends OptionMenu{
+
+    public static void main(String[] args) throws  IOException {
+        OptionMenu optionMenu = new OptionMenu();
+
+        optionMenu.getLogin();
+    }
+
+}
